78488050c01843ad68d5dffb8dcb00abb35ed74c
Floris <floris.heyvaert@gmail.com>
2024-08-13 22:13:13 +0200

    added box shadow

--- src/dotnet/ReStack.Common/Models/StackModel.cs
+++ src/dotnet/ReStack.Common/Models/StackModel.cs
@@ -19,26 +19,6 @@ public class StackModel : IEquatable<StackModel>
     public JobModel LastJob { get => Jobs.OrderByDescending(x => x.Sequence).FirstOrDefault(); }
     public JobModel RunningJob { get => Jobs.Where(x => x.State == JobState.Running.ToString() || x.State == JobState.Queued.ToString()).OrderByDescending(x => x.Sequence).FirstOrDefault(); }
 
-    // todo not the correct place because it's a 3th library
-    public string GetIcon()
-    {
-        return GetIcon(Type);
-    }
-
-    public static string GetIcon(string type)
-    {
-        var icon = type;
-
-        if (type == ProgrammingLanguage.Shell.ToString())
-            icon = "img/languages/shell.svg";
-        else if (type == ProgrammingLanguage.PowerShell.ToString())
-            icon = "img/languages/powershell.svg";
-        else if (type == ProgrammingLanguage.Bat.ToString())
-            icon = "img/languages/bat.svg";
-
-        return icon == type ? icon : $"<img src='{icon}' style='height: 1.25rem; width: 1.25rem;' />";
-    }
-
     public bool Equals(StackModel other)
     {
         return other?.Id == Id;
