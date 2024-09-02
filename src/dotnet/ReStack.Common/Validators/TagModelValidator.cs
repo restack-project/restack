04698ffbb5d24e78f23a719299e32b2eff74f7fd
Floris <floris.heyvaert@gmail.com>
2024-09-02 12:18:58 +0200

    duplicate tag name

--- src/dotnet/ReStack.Common/Validators/TagModelValidator.cs
+++ src/dotnet/ReStack.Common/Validators/TagModelValidator.cs
@@ -1,12 +1,23 @@
 ﻿using FluentValidation;
+using ReStack.Common.Interfaces;
 using ReStack.Common.Models;
 
 namespace ReStack.Common.Validators;
 
 public class TagModelValidator : AbstractValidator<TagModel>
 {
-    public TagModelValidator()
+    private readonly IReStackDbContext _context;
+
+    public TagModelValidator(IReStackDbContext context)
     {
+        _context = context;
+
         RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Name).Must((x, y) => !HasDuplicateName(x)).WithMessage(x => "Name already exists.");
+    }
+
+    private bool HasDuplicateName(TagModel model)
+    {
+        return _context.Tag.Where(x => x.Id != model.Id).Any(x => x.Name.ToLower() == model.Name.ToLower());
     }
 }
